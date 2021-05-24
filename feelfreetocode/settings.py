2da68d428a2e8339352a5f3c9379b0d30be3fa50
virendra <patelvirendra62@gmail.com>
2021-05-24 14:57:14 +0530

    urls.py changed

--- feelfreetocode/settings.py
+++ feelfreetocode/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'course',
     'chapter',
     'coupon',
@@ -70,9 +71,18 @@ WSGI_APPLICATION = 'feelfreetocode.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'feelfreetocode',
+        'PORT': 3306,
+        'HOST': 'localhost',
+        "USER": 'root',
+        "PASSWORD": 'root'
     }
 }
 
