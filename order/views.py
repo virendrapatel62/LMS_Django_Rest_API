78c1fe9faa19ffc70502bb9d46ac34361261c03b
virendra <patelvirendra62@gmail.com>
2021-06-03 16:51:09 +0530

    couppm validaiton

--- order/views.py
+++ order/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-from order.serializers import OrderItemSerializer, OrderSerializer, SubscriptionSerializer
+from order.serializers import OrderCreateSerializer, OrderItemSerializer, OrderSerializer, SubscriptionSerializer
 from rest_framework.permissions import IsAuthenticated
 from coupon.models import Coupon
 
@@ -17,15 +17,9 @@ class CreateOrderApiView(APIView):
 
         data = JSONParser().parse(request)
         print(data, data)
-        course = data.get('course')
-        courses = data.get('courses')
-        coupon_code = data.get('coupon_code')
-        coupon = None
-
-        if coupon_code:
-            try:
-                coupon = Coupon.objects.get(code=coupon_code)
-            except Coupon.DoesNotExist:
-                return Response({"coupon": ["Coupon is not valid"]}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(data)
+        serializer = OrderCreateSerializer(data=data)
+        if serializer.is_valid():
+            print("Validated Data", serializer.validated_data)
+            return Response('ok')
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
