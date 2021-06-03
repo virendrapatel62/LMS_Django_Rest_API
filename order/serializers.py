5d875d24ebfe4e6fabbd8b94b490a6f0e4c01226
virendra <patelvirendra62@gmail.com>
2021-06-03 15:58:14 +0530

    COupon Validation

--- order/serializers.py
+++ order/serializers.py
@@ -1,3 +1,5 @@
+from core.serializers import UserSerializer
+from coupon.serializers import CouponSerializer
 from django.db.models.base import Model
 from order import models
 from rest_framework.serializers import ModelSerializer, CharField, UUIDField
@@ -5,7 +7,8 @@ from rest_framework.serializers import ModelSerializer, CharField, UUIDField
 
 class OrderSerializer(ModelSerializer):
     order_id = CharField(max_length=30, required=False)
-    user = UUIDField(required=False)
+    user = UserSerializer(read_only=True)
+    coupon = CouponSerializer(read_only=True)
 
     class Meta:
         model = models.Order
--- order/views.py
+++ order/views.py
@@ -7,16 +7,25 @@ from rest_framework.parsers import JSONParser
 from rest_framework import status
 from order.serializers import OrderItemSerializer, OrderSerializer, SubscriptionSerializer
 from rest_framework.permissions import IsAuthenticated
+from coupon.models import Coupon
 
 
 class CreateOrderApiView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        print(request.user)
+
         data = JSONParser().parse(request)
-        orderSerializer = OrderSerializer(data=data)
-        if orderSerializer.is_valid():
-            return Response("Ok")
-        else:
-            return Response(orderSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        print(data, data)
+        course = data.get('course')
+        courses = data.get('courses')
+        coupon_code = data.get('coupon_code')
+        coupon = None
+
+        if coupon_code:
+            try:
+                coupon = Coupon.objects.get(code=coupon_code)
+            except Coupon.DoesNotExist:
+                return Response({"coupon": ["Coupon is not valid"]}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(data)
