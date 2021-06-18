e2791d3ab1ac8f86c5ea8d099cfb6d87af964f2c
virendra <patelvirendra62@gmail.com>
2021-06-18 15:18:34 +0530

    doubt

--- doubt/views.py
+++ doubt/views.py
@@ -6,10 +6,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import BasePermission
 # Create your views here.
 
 
+class CanDeleteAndUpdateOnlyOwnDoubt(BasePermission):
+    def has_permission(self, request, view):
+        method = request.method
+        if method in ['PUT', "DELETE", "PATCH"]:
+            user = request.user
+            if user.is_superuser:
+                return True
+
+            doubt_id = view.kwargs.get('pk')
+            try:
+                doubt = Doubt.objects.get(pk=doubt_id)
+            except Doubt.DoesNotExist:
+                return Response({"details": "doubt not found"}, status=400)
+            return doubt.user == user
+
+        return True
+
+
 class DoubtModelViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly,
+                          CanDeleteAndUpdateOnlyOwnDoubt]
     queryset = Doubt.objects.all()
     serializer_class = DoubtSerializer
