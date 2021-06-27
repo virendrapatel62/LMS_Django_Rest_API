74b721d3eba08fdaffb9ebe0a9c56881d013fa05
virendra <patelvirendra62@gmail.com>
2021-06-27 19:31:38 +0530

    env

--- feelfreetocode/urls.py
+++ feelfreetocode/urls.py
@@ -32,4 +32,6 @@ urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+
 ]
